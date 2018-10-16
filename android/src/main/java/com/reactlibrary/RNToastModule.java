c69022c7048fd9561c9929a9b69e56a1f84668d4
Bunlong <bunlong.van@gmail.com>
2018-10-16 15:03:57 +0700

    Create Toast on Android

--- android/src/main/java/com/reactlibrary/RNToastModule.java
+++ android/src/main/java/com/reactlibrary/RNToastModule.java
@@ -6,9 +6,18 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import android.widget.Toast;
+
+import java.util.Map;
+import java.util.HashMap;
+
 public class RNToastModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
+
+  // Bunlong
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
 
   public RNToastModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -18,5 +27,20 @@ public class RNToastModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNToast";
+  }
+
+  // Bunlong
+	@Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+    constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+    return constants;
+  }
+
+  // Bunlong
+  @ReactMethod
+  public void show(String message, int duration) {
+    Toast.makeText(getReactApplicationContext(), message, duration).show();
   }
 }
